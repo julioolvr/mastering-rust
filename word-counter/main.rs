0c67d10b6d213fb523428c229d72484f74b8aeba
Julio Olivera <julio.olvr@gmail.com>
2017-06-19 21:54:20 -0300

    Code compiling

--- word-counter/main.rs
+++ word-counter/main.rs
@@ -2,16 +2,17 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::BufRead;
 use std::io::BufReader;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct WordStore(HashMap<String, u64>);
 
 impl WordStore {
-  fn new() {
+  fn new() -> WordStore {
     WordStore(HashMap::new())
   }
 
-  fn increment(word: &str) {
+  fn increment(&mut self, word: &str) {
     let key = word.to_string();
     let count = self.0.entry(key).or_insert(0);
     *count += 1;
@@ -32,11 +33,12 @@ fn main() {
   let file = File::open(filename).expect("Could not open file");
   let reader = BufReader::new(file);
 
-  let word_store = WordStore::new();
+  let mut word_store = WordStore::new();
 
   for line in reader.lines() {
     let line = line.expect("Could not read line");
     let words = line.split(" ");
+
     for word in words {
       if word == "" {
         continue;
@@ -44,7 +46,7 @@ fn main() {
         word_store.increment(word);
       }
     }
-
-    word_store.display();
   }
+
+  word_store.display();
 }
