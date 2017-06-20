c5372921ce0b7a4485bdd7e6a26426d9acf7d650
Julio Olivera <julio.olvr@gmail.com>
2017-06-20 19:35:20 -0300

    Move being successfully

--- fanstr-buigam/src/main.rs
+++ fanstr-buigam/src/main.rs
@@ -64,63 +64,83 @@ impl Grid {
         }
     }
 
-    fn move_being_in_coord(&self,
+    fn take_being_from_square(&mut self, coord: (usize, usize)) -> Being {
+        let mut square = self.squares.get_mut(coord.0 + coord.1 * self.size.1).unwrap();
+        let being = square.beings.take();
+        being.unwrap()
+    }
+
+    fn put_being_in_square(&mut self, coord: (usize, usize), being: Being) {
+        let mut square = self.squares.get_mut(coord.0 + coord.1 * self.size.1).unwrap();
+        square.beings = Some(being);
+    }
+
+    fn move_being_in_coord(&mut self,
                            coord: (usize, usize),
                            dir: Direction)
                            -> Result<(usize, usize), MovementError> {
+        let mut new_x;
+        let mut new_y;
 
-        let origin_square = self.squares
-            .get(coord.0 * self.size.0 + coord.1)
-            .expect("Index out of bounds trying to get being");
+        {
+            let origin_square = self.squares
+                .get(coord.0 + coord.1 * self.size.1)
+                .expect("Index out of bounds trying to get being");
 
-        match origin_square.beings {
-            Some(ref being) => being,
-            None => return Err(MovementError::NoBeingInSquare),
-        };
+            match origin_square.beings {
+                Some(ref being) => being,
+                None => return Err(MovementError::NoBeingInSquare),
+            };
 
-        let new_x = match dir {
-            Direction::East => coord.0 + 1,
-            Direction::West => {
-                if let Some(val) = coord.0.checked_sub(1) {
-                    val
-                } else {
-                    return Err(MovementError::OutOfBounds);
+            new_x = match dir {
+                Direction::East => coord.0 + 1,
+                Direction::West => {
+                    if let Some(val) = coord.0.checked_sub(1) {
+                        val
+                    } else {
+                        return Err(MovementError::OutOfBounds);
+                    }
+                }
+                _ => coord.0,
+            };
+
+            new_y = match dir {
+                Direction::South => coord.1 + 1,
+                Direction::North => {
+                    if let Some(val) = coord.1.checked_sub(1) {
+                        val
+                    } else {
+                        return Err(MovementError::OutOfBounds);
+                    }
+                }
+                _ => coord.1,
+            };
+
+            if new_x >= self.size.0 || new_y >= self.size.1 {
+                return Err(MovementError::OutOfBounds);
+            }
+
+            let destination_square = self.squares
+                .get(new_x + new_y * self.size.1)
+                .expect("Index out of bounds trying to get being");
+
+            if destination_square.beings.is_some() {
+                return Err(MovementError::BeingInDestinationSquare);
+            }
+
+            if let Some(ref block) = destination_square.block {
+                if *block == TerrainBlock::Stone {
+                    return Err(MovementError::StoneInDestinationSquare);
                 }
             }
-            _ => coord.0,
-        };
-
-        let new_y = match dir {
-            Direction::South => coord.1 + 1,
-            Direction::North => {
-                if let Some(val) = coord.1.checked_sub(1) {
-                    val
-                } else {
-                    return Err(MovementError::OutOfBounds);
-                }
-            }
-            _ => coord.1,
-        };
-
-        if new_x >= self.size.0 || new_y >= self.size.1 {
-            return Err(MovementError::OutOfBounds);
         }
 
-        let destination_square = self.squares
-            .get(coord.0 * self.size.0 + coord.1)
-            .expect("Index out of bounds trying to get being");
+        // So far so good, let's mutate everything
+        let being = self.take_being_from_square(coord);
+        let new_coords = (new_x, new_y);
+        self.put_being_in_square(new_coords, being);
 
-        if destination_square.beings.is_some() {
-            return Err(MovementError::BeingInDestinationSquare);
-        }
-
-        if let Some(ref block) = destination_square.block {
-            if *block == TerrainBlock::Stone {
-                return Err(MovementError::StoneInDestinationSquare);
-            }
-        }
-
-        Ok((new_x, new_y))
+        Ok(new_coords)
     }
 }
 
@@ -149,7 +169,7 @@ mod tests {
 
     #[test]
     fn test_move_being_without_being_in_square() {
-        let grid = ::Grid::generate_empty(3, 3);
+        let mut grid = ::Grid::generate_empty(3, 3);
         assert_eq!(grid.move_being_in_coord((0, 0), ::Direction::West),
                    Err(::MovementError::NoBeingInSquare));
     }
@@ -185,6 +205,16 @@ mod tests {
         grid.squares[0].beings = Some(::Being::Human {});
         grid.squares[1].block = Some(::TerrainBlock::Stone);
         assert_eq!(grid.move_being_in_coord((0, 0), ::Direction::East),
-                   Err(::MovementError::BeingInDestinationSquare));
+                   Err(::MovementError::StoneInDestinationSquare));
+    }
+
+    #[test]
+    fn test_move_successfully() {
+        let mut grid = ::Grid::generate_empty(3, 3);
+        grid.squares[0].beings = Some(::Being::Human {});
+        let result = grid.move_being_in_coord((0, 0), ::Direction::East);
+        assert!(result.is_ok());
+        assert!(grid.squares[0].beings.is_none());
+        assert!(grid.squares[1].beings.is_some());
     }
 }
