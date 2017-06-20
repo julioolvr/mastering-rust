3118040d0ce2362424479ee8084e6bcd3ccd758b
Julio Olivera <julio.olvr@gmail.com>
2017-06-20 18:25:04 -0300

    Being movement with error for missing or moving out of grid

--- fanstr-buigam/src/main.rs
+++ fanstr-buigam/src/main.rs
@@ -20,6 +20,19 @@ enum Being {
     Human,
 }
 
+enum Direction {
+    West,
+    East,
+    North,
+    South,
+}
+
+#[derive(Debug, PartialEq)]
+enum MovementError {
+    NoBeingInSquare,
+    OutOfBounds,
+}
+
 struct Square {
     ground: TerrainGround,
     block: Option<TerrainBlock>,
@@ -38,16 +51,60 @@ impl Grid {
 
         for _ in 0..number_of_squares {
             squares.push(Square {
-                ground: TerrainGround::Soil,
-                block: None,
-                beings: None,
-            });
+                             ground: TerrainGround::Soil,
+                             block: None,
+                             beings: None,
+                         });
         }
 
         Grid {
             size: (size_x, size_y),
             squares: squares,
         }
+    }
+
+    fn move_being_in_coord(&self,
+                           coord: (usize, usize),
+                           dir: Direction)
+                           -> Result<(usize, usize), MovementError> {
+        let square = self.squares
+            .get(coord.0 * self.size.0 + coord.1)
+            .expect("Index out of bounds trying to get being");
+
+        match square.beings {
+            Some(ref being) => being,
+            None => return Err(MovementError::NoBeingInSquare),
+        };
+
+        let new_x = match dir {
+            Direction::West => coord.0 + 1,
+            Direction::East => {
+                if let Some(val) = coord.0.checked_sub(1) {
+                    val
+                } else {
+                    return Err(MovementError::OutOfBounds);
+                }
+            }
+            _ => coord.0,
+        };
+
+        let new_y = match dir {
+            Direction::South => coord.1 + 1,
+            Direction::North => {
+                if let Some(val) = coord.1.checked_sub(1) {
+                    val
+                } else {
+                    return Err(MovementError::OutOfBounds);
+                }
+            }
+            _ => coord.1,
+        };
+
+        if new_x >= self.size.0 || new_y >= self.size.1 {
+            return Err(MovementError::OutOfBounds);
+        }
+
+        Ok((new_x, new_y))
     }
 }
 
@@ -72,5 +129,28 @@ mod tests {
 
         assert_eq!(grid.squares.len(), 5 * 13);
         assert_eq!(number_of_squares, 5 * 13);
+    }
+
+    #[test]
+    fn test_move_being_without_being_in_square() {
+        let grid = ::Grid::generate_empty(3, 3);
+        assert_eq!(grid.move_being_in_coord((0, 0), ::Direction::West),
+                   Err(::MovementError::NoBeingInSquare));
+    }
+
+    #[test]
+    fn test_move_being_out_of_grids_beginning() {
+        let mut grid = ::Grid::generate_empty(3, 3);
+        grid.squares[0].beings = Some(::Being::Human {});
+        assert_eq!(grid.move_being_in_coord((0, 0), ::Direction::North),
+                   Err(::MovementError::OutOfBounds));
+    }
+
+    #[test]
+    fn test_move_being_out_of_grids_end() {
+        let mut grid = ::Grid::generate_empty(3, 3);
+        grid.squares[3 * 2 + 2].beings = Some(::Being::Human {});
+        assert_eq!(grid.move_being_in_coord((2, 2), ::Direction::West),
+                   Err(::MovementError::OutOfBounds));
     }
 }
